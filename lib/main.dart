4696b7d4869bede45c6345d59d28e897c5f11883
Seph98m <baylon.markjoseph@marsu.edu.ph>
2026-06-11 15:37:46 +0800

    commit1

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ void main() async {
           const MyApp({super.key});
 
             @override
-              Widget build(BuildContext context) {
+              Widget build(BuildContext context)
+               {
                   return MaterialApp(
                     title: 'AppScale',
                     debugShowCheckedModeBanner: false,
